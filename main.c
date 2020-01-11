1432ddab90209435673efa49a9679f58362f7703
Blake Knight <oss.ideas@gmail.com>
2020-01-11 13:46:32 -0600

    add initial .clang-format & apply format

--- main.c
+++ main.c
@@ -1,41 +1,37 @@
 #include <CoreFoundation/CoreFoundation.h>
-#include <objc/objc.h>
 #include <objc/objc-runtime.h>
+#include <objc/objc.h>
 #include <stdio.h>
 
 #include "utils/class-info.h"
 
-int main(int argc, char **argv)
-{
-    Class NSUserNotification = objc_getClass("NSUserNotification");
-    Class NSUserNotificationCenter = objc_getClass("NSUserNotificationCenter");
+int main(int argc, char **argv) {
+	Class NSUserNotification = objc_getClass("NSUserNotification");
+	Class NSUserNotificationCenter = objc_getClass("NSUserNotificationCenter");
 
-    id userNotification = class_createInstance(NSUserNotification, 0);
-    id userNotificationCenter =
-        class_createInstance(NSUserNotificationCenter, 0);
+	id userNotification = class_createInstance(NSUserNotification, 0);
+	id userNotificationCenter =
+	    class_createInstance(NSUserNotificationCenter, 0);
 
-    SEL scheduleNotificationSelector = sel_registerName("scheduleNotification:");
+	SEL scheduleNotificationSelector =
+	    sel_registerName("scheduleNotification:");
 
-    Method scheduleNotification = class_getInstanceMethod(
-        NSUserNotificationCenter,
-        scheduleNotificationSelector);
+	Method scheduleNotification = class_getInstanceMethod(
+	    NSUserNotificationCenter, scheduleNotificationSelector);
 
-    // objc_property_t _Nullable z = class_getProperty(NSUserNotificationCenter, "defaultUserNotificationCenter");
+	// objc_property_t _Nullable z = class_getProperty(NSUserNotificationCenter,
+	// "defaultUserNotificationCenter");
 
-    // userNotification.title = title;
-    // userNotification.subtitle = subtitle;
-    // userNotification.informativeText = message;
-    // userNotification.userInfo = options;
+	// userNotification.title = title;
+	// userNotification.subtitle = subtitle;
+	// userNotification.informativeText = message;
+	// userNotification.userInfo = options;
 
-    object_setInstanceVariable(
-        userNotification,
-        "title",
-        (void *_Nullable)CFSTR("Test title"));
+	object_setInstanceVariable(userNotification, "title",
+	                           (void *_Nullable)CFSTR("Test title"));
 
-    printClassInfo(NSUserNotificationCenter);
+	printClassInfo(NSUserNotificationCenter);
 
-    method_invoke(
-        userNotificationCenter,
-        scheduleNotification,
-        userNotification);
+	method_invoke(userNotificationCenter, scheduleNotification,
+	              userNotification);
 }
--- utils/class-info.h
+++ utils/class-info.h
@@ -1,67 +1,57 @@
-#include <objc/objc.h>
 #include <objc/objc-runtime.h>
+#include <objc/objc.h>
 #include <stdio.h>
 
-void printInstanceVariables(Class class)
-{
-    uint iVarCount;
-    Ivar *_Nullable iVarList = class_copyIvarList(class, &iVarCount);
+void printInstanceVariables(Class class) {
+  uint iVarCount;
+  Ivar *_Nullable iVarList = class_copyIvarList(class, &iVarCount);
 
-    for (int i = 0; i < iVarCount; i++)
-    {
-        const char *_Nullable iVarName = ivar_getName(iVarList[i]);
-        const char *_Nullable iVarType = ivar_getTypeEncoding(iVarList[i]);
-        printf("%s (%s)\n", iVarName, iVarType);
-    }
+  for (int i = 0; i < iVarCount; i++) {
+	const char *_Nullable iVarName = ivar_getName(iVarList[i]);
+	const char *_Nullable iVarType = ivar_getTypeEncoding(iVarList[i]);
+	printf("%s (%s)\n", iVarName, iVarType);
+  }
 }
 
-void printMethodNames(Class class)
-{
-    uint methodCount;
-    Method *methodList = class_copyMethodList(class, &methodCount);
+void printMethodNames(Class class) {
+  uint methodCount;
+  Method *methodList = class_copyMethodList(class, &methodCount);
 
-    for (int i = 0; i < methodCount; i++)
-    {
-        SEL selector = method_getName(methodList[i]);
-        const char *_Nonnull name = sel_getName(selector);
-        printf("%s\n", name);
-    }
+  for (int i = 0; i < methodCount; i++) {
+	SEL selector = method_getName(methodList[i]);
+	const char *_Nonnull name = sel_getName(selector);
+	printf("%s\n", name);
+  }
 }
 
-void printPropertyNames(Class class)
-{
-    uint propertyCount;
-    objc_property_t *propertyList = class_copyPropertyList(class, &propertyCount);
+void printPropertyNames(Class class) {
+  uint propertyCount;
+  objc_property_t *propertyList = class_copyPropertyList(class, &propertyCount);
 
-    for (int i = 0; i < propertyCount; i++)
-    {
-        const char *_Nonnull propertyName = property_getName(propertyList[i]);
-        const char *_Nullable propertyType = property_getAttributes(propertyList[i]);
-        printf("%s (%s)\n", propertyName, propertyType);
-    }
+  for (int i = 0; i < propertyCount; i++) {
+	const char *_Nonnull propertyName = property_getName(propertyList[i]);
+	const char *_Nullable propertyType =
+	    property_getAttributes(propertyList[i]);
+	printf("%s (%s)\n", propertyName, propertyType);
+  }
 }
 
-void printMethodArgumentInfo(Method method)
-{
-    char *name = (char *)method_getName(method);
-    uint argc = method_getNumberOfArguments(method);
+void printMethodArgumentInfo(Method method) {
+  char *name = (char *)method_getName(method);
+  uint argc = method_getNumberOfArguments(method);
 
-    printf("%s\n", name);
+  printf("%s\n", name);
 
-    for (uint i = 0; i < argc; i++)
-    {
-        printf(
-            "  - %s\n",
-            method_copyArgumentType(method, i));
-    }
+  for (uint i = 0; i < argc; i++) {
+	printf("  - %s\n", method_copyArgumentType(method, i));
+  }
 }
 
-void printClassInfo(Class class)
-{
-    puts("METHODS\n====================");
-    printMethodNames(class);
-    puts("\nPROPERTIES\n====================");
-    printPropertyNames(class);
-    puts("\nINSTANCE VARIABLES\n====================");
-    printInstanceVariables(class);
+void printClassInfo(Class class) {
+  puts("METHODS\n====================");
+  printMethodNames(class);
+  puts("\nPROPERTIES\n====================");
+  printPropertyNames(class);
+  puts("\nINSTANCE VARIABLES\n====================");
+  printInstanceVariables(class);
 }
